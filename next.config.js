27866f1807a47b2a2b163c23a80c06c5f0cb0b3f
二冰 <1457586430@qq.com>
2025-01-06 02:45:56 +0800

    revert: 回退到基础版本 - 移除缓存控制 - 移除 middleware - 简化 next.config.js - 恢复基础 Notion 集成

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@
 const nextConfig = {
   images: {
     domains: ['www.notion.so', 'prod-files-secure.s3.us-west-2.amazonaws.com'],
-  },
-  // 禁用页面缓存
-  staticPageGenerationTimeout: 1000,
-  experimental: {
-    workerThreads: true,
-    cpus: 1
   }
 }
 
