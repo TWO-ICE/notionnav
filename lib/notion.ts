de7d51657a8fa99422187e492aed6cf9d6ae9078
二冰 <1457586430@qq.com>
2025-01-06 15:26:57 +0800

    自动提交于 2025-01-06 15:26:57

--- lib/notion.ts
+++ lib/notion.ts
@@ -1,5 +1,9 @@
 import { Client } from '@notionhq/client';
-import { PageObjectResponse } from '@notionhq/client/build/src/api-endpoints';
+import { 
+  PageObjectResponse,
+  GetDatabaseResponse,
+  DatabaseObjectResponse
+} from '@notionhq/client/build/src/api-endpoints';
 
 const notion = new Client({
   auth: process.env.NOTION_API_KEY,
@@ -51,16 +55,30 @@ export async function getLinks(): Promise<Link[]> {
 
 export async function getDatabaseInfo() {
   try {
-    const database = await notion.databases.retrieve({
+    const response = await notion.databases.retrieve({
       database_id: process.env.NOTION_DATABASE_ID!,
     });
+    
+    // 类型断言为完整的数据库对象
+    const database = response as DatabaseObjectResponse;
 
-    return {
-      icon: database.icon?.type === 'external' ? database.icon.external.url : 
-            database.icon?.type === 'file' ? database.icon.file.url : undefined,
-      cover: database.cover?.type === 'external' ? database.cover.external.url :
-             database.cover?.type === 'file' ? database.cover.file.url : undefined
-    };
+    // 处理图标
+    let icon: string | undefined;
+    if (database.icon?.type === 'external') {
+      icon = database.icon.external.url;
+    } else if (database.icon?.type === 'file') {
+      icon = database.icon.file.url;
+    }
+
+    // 处理封面
+    let cover: string | undefined;
+    if (database.cover?.type === 'external') {
+      cover = database.cover.external.url;
+    } else if (database.cover?.type === 'file') {
+      cover = database.cover.file.url;
+    }
+
+    return { icon, cover };
   } catch (error) {
     console.error('Error fetching database info:', error);
     return {
