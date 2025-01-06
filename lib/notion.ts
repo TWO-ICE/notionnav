8c11b7ce60afcf3fc66fea55b4edd273c68eed04
二冰 <1457586430@qq.com>
2025-01-06 15:08:19 +0800

    自动提交于 2025-01-06 15:08:19

--- lib/notion.ts
+++ lib/notion.ts
@@ -1,9 +1,25 @@
 import { Client } from '@notionhq/client';
-import { DatabaseObjectResponse, PageObjectResponse } from '@notionhq/client/build/src/api-endpoints';
+import { 
+  DatabaseObjectResponse, 
+  PageObjectResponse,
+  RichTextItemResponse,
+  SelectPropertyItemObjectResponse,
+  TitlePropertyItemObjectResponse,
+  URLPropertyItemObjectResponse,
+  FilesPropertyItemObjectResponse
+} from '@notionhq/client/build/src/api-endpoints';
 
 const notion = new Client({
   auth: process.env.NOTION_API_KEY,
 });
+
+interface NotionProperties {
+  title: TitlePropertyItemObjectResponse;
+  desp: { type: "rich_text"; rich_text: Array<RichTextItemResponse> };
+  cat: SelectPropertyItemObjectResponse;
+  icon: FilesPropertyItemObjectResponse;
+  link: URLPropertyItemObjectResponse;
+}
 
 export async function getLinks() {
   const response = await notion.databases.query({
@@ -12,14 +28,15 @@ export async function getLinks() {
 
   return response.results.map((page) => {
     const pageObj = page as PageObjectResponse;
+    const properties = pageObj.properties as unknown as NotionProperties;
+
     return {
       id: pageObj.id,
-      title: pageObj.properties.title?.title[0]?.plain_text || '',
-      description: pageObj.properties.desp?.rich_text[0]?.plain_text || '',
-      category: pageObj.properties.cat?.select?.name || '',
-      icon: pageObj.properties.icon?.files[0]?.file?.url || 
-            pageObj.properties.icon?.files[0]?.external?.url || '',
-      link: pageObj.properties.link?.url || '',
+      title: properties.title.title[0]?.plain_text || '',
+      description: properties.desp.rich_text[0]?.plain_text || '',
+      category: properties.cat.select?.name || '',
+      icon: properties.icon.files[0]?.file?.url || properties.icon.files[0]?.external?.url || '',
+      link: properties.link.url || '',
     };
   });
 }
@@ -29,13 +46,10 @@ export async function getDatabaseInfo() {
     database_id: process.env.NOTION_DATABASE_ID!
   }) as DatabaseObjectResponse;
 
-  const icon = database.icon;
-  const cover = database.cover;
-
   return {
-    icon: icon?.type === 'external' ? icon.external.url : 
-          icon?.type === 'file' ? icon.file.url : undefined,
-    cover: cover?.type === 'external' ? cover.external.url :
-           cover?.type === 'file' ? cover.file.url : undefined
+    icon: database.icon?.type === 'external' ? database.icon.external.url : 
+          database.icon?.type === 'file' ? database.icon.file.url : undefined,
+    cover: database.cover?.type === 'external' ? database.cover.external.url :
+           database.cover?.type === 'file' ? database.cover.file.url : undefined
   };
 } 
