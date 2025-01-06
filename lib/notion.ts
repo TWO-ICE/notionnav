11c63a76fdab03cb1ba86b5c4c25719141153a5c
二冰 <1457586430@qq.com>
2025-01-06 15:25:05 +0800

    自动提交于 2025-01-06 15:25:05

--- lib/notion.ts
+++ lib/notion.ts
@@ -1,19 +1,10 @@
 import { Client } from '@notionhq/client';
-import { 
-  DatabaseObjectResponse, 
-  PageObjectResponse,
-  RichTextItemResponse,
-  SelectPropertyItemObjectResponse,
-  TitlePropertyItemObjectResponse,
-  UrlPropertyItemObjectResponse,
-  FilesPropertyItemObjectResponse
-} from '@notionhq/client/build/src/api-endpoints';
+import { PageObjectResponse } from '@notionhq/client/build/src/api-endpoints';
 
 const notion = new Client({
   auth: process.env.NOTION_API_KEY,
 });
 
-// 导出 Link 接口以供其他文件使用
 export interface Link {
   id: string;
   title: string;
@@ -23,40 +14,33 @@ export interface Link {
   link: string;
 }
 
-interface NotionProperties {
-  title: TitlePropertyItemObjectResponse;
-  desp: { type: "rich_text"; rich_text: Array<RichTextItemResponse> };
-  cat: SelectPropertyItemObjectResponse;
-  icon: FilesPropertyItemObjectResponse;
-  link: UrlPropertyItemObjectResponse;
-}
-
 export async function getLinks(): Promise<Link[]> {
   try {
     const response = await notion.databases.query({
       database_id: process.env.NOTION_DATABASE_ID!,
     });
 
-    const links = response.results.map((page) => {
-      try {
-        const pageObj = page as PageObjectResponse;
-        const properties = pageObj.properties as unknown as NotionProperties;
+    const links = response.results
+      .filter((page): page is PageObjectResponse => 'properties' in page)
+      .map((page) => {
+        try {
+          const properties = page.properties as any;
 
-        const link: Link = {
-          id: pageObj.id,
-          title: properties.title.title[0]?.plain_text || '',
-          description: properties.desp.rich_text[0]?.plain_text || '',
-          category: properties.cat.select?.name || '',
-          icon: properties.icon.files[0]?.file?.url || properties.icon.files[0]?.external?.url || '',
-          link: properties.link.url || '',
-        };
-
-        return link;
-      } catch (error) {
-        console.error('Error processing page:', error);
-        return null;
-      }
-    }).filter((link): link is Link => link !== null);
+          return {
+            id: page.id,
+            title: properties.title?.title?.[0]?.plain_text || '',
+            description: properties.desp?.rich_text?.[0]?.plain_text || '',
+            category: properties.cat?.select?.name || '',
+            icon: properties.icon?.files?.[0]?.file?.url || 
+                  properties.icon?.files?.[0]?.external?.url || '',
+            link: properties.link?.url || '',
+          };
+        } catch (error) {
+          console.error('Error processing page:', error);
+          return null;
+        }
+      })
+      .filter((link): link is Link => link !== null);
 
     return links;
   } catch (error) {
@@ -68,8 +52,8 @@ export async function getLinks(): Promise<Link[]> {
 export async function getDatabaseInfo() {
   try {
     const database = await notion.databases.retrieve({
-      database_id: process.env.NOTION_DATABASE_ID!
-    }) as DatabaseObjectResponse;
+      database_id: process.env.NOTION_DATABASE_ID!,
+    });
 
     return {
       icon: database.icon?.type === 'external' ? database.icon.external.url : 
--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['www.notion.so', 'prod-files-secure.s3.us-west-2.amazonaws.com'],
-  }
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.notion.so',
+      },
+      {
+        protocol: 'https',
+        hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig 
